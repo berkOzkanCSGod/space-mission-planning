5ca589814bb9232a6aed2446bb3240354552b271
mert.fidan715 <mert.fidan@ugbilkent.edu.tr>
2024-04-25 14:15:25 +0300

    methods for trainings

--- apollo1/models.py
+++ apollo1/models.py
@@ -82,6 +82,12 @@ class Astronaut(models.Model):
                 return 'company'
             else:
                 return None
+            
+    @classmethod
+    def getAstronautTrainings(cls, id):
+        with connection.cursor() as sql:
+            sql.execute("SELECT T.t_id, T.t_name, T.t_description, T.t_status FROM training T, completes C WHERE C.astro_id=%s, C.t_id = T.t_id", [id])
+            return sql.fetchall()
 
     @classmethod
     def getUserById(cls, id):
@@ -358,3 +364,7 @@ class Space_Mission(models.Model):
     def updateStatus(sm_id, status):
         with connection.cursor() as sql:
             sql.execute("UPDATE performing_missions SET status = %s WHERE sm_id = %s", [status,sm_id])
+    def getRequiredTrainings(cls, id):
+        with connection.cursor() as sql:
+            sql.execute("SELECT T.t_id, T.t_name, T.t_description, T.t_status FROM training T, required R WHERE R.sm_id=%s, R.t_id = T.t_id", [id])
+            return sql.fetchall()
