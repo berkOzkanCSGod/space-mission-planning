73a8067d493c4e488bd298ba40a95a61b1183a00
mkaganozsoy <mkaganozsoy@gmail.com>
2024-05-18 18:57:17 +0300

    deleted print statements

--- apollo1/views.py
+++ apollo1/views.py
@@ -469,13 +469,13 @@ def get_filtered_transactions(request):
 
     # Retrieve transactions based on filters
     transactions = Transaction.getFilteredTransactions(user_id, date, amount_less_than, amount_greater_than)
-    print(transactions)
+
     # add company name to each transaction
     updated_transactions = []
     for t in transactions:
         sender_name = Bank_Account.getCompanyNameById(t[1])
         receiver_name = Bank_Account.getCompanyNameById(t[2])
         updated_transactions.append((t[0], sender_name, receiver_name, t[3], t[4]))
-    print(updated_transactions)
+
     # Render the transactions to the template
     return render(request, "transactions.html", {'transactions': updated_transactions})
